4ad66d75ae9d78361926b24c44dbbff64b91cfc3
Luish-ar <luisrapper111@gmail.com>
2026-03-01 11:21:05 -0300

    Co-authored-by: Luan Gomes <luangrezende@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-// Select elements
 const btn1 = document.getElementById('btn1');
-const btn2 = document.getElementById('btn2');
-const message = document.getElementById('message');
 
-// Add events to buttons
 btn1.addEventListener('click', function() {
-    message.textContent = 'You clicked the first button!';
-    alert('Button 1 was clicked!');
-    var name = prompt('Please enter your name:');
-    if (name) {
-        message.textContent = `Hello, ${name}! You clicked the first button!`;
-        console.log(`User entered name: ${name}`);
-    }
+    textBox.textContent = 'perdeu!';
 });
 
 btn2.addEventListener('click', function() {
-    message.textContent = 'You clicked the second button!';
+    textBox.textContent = 'Ganhou!';
 });
+
+btn3.addEventListener('click', function() {
+    textBox.textContent = 'Eu avisei!';
+    console.log('Cuidado com o Gomes!');
+    console.error('Cuidado com o Gomes!');
+    console.warn('Cuidado com o Gomes!');
+    alert('Cuidado com o Gomes!');
+});
+
+// adicionar evento para mudar titulo, subtitulo e texto do paragrafo
+btn4.addEventListener('click', function() {
+    titulo.textContent = 'Verdades do Gomes';
+    textBox.textContent = 'Duvido';
+    subtitulo.textContent = 'mudar!';
+});
+
+// adicionar evento no botao5 para quando clicar, mostrar um prompt (alert()) com o texto digitado
+btn5.addEventListener('click', function() {
+    var input = inputText.value;
+    console.log(input);
+    alert('Salvo!');
+});
+  
---
+++ script2.js
@@ -0,0 +1,12 @@
+const btn1 = document.getElementById('btn1');
+const message = document.getElementById('message');
+
+// btn1.addEventListener('click', function() {
+//     message.textContent = 'perdeu!';
+//     alert('Button 1 was clicked!');
+//     var name = prompt('Please enter your name:');
+//     if (name) {
+//         message.textContent = `Hello, ${name}! You clicked the first button!`;
+//         console.log(`User entered name: ${name}`);
+//     }
+// });
